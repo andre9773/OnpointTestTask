5338af7fd66d6674ba5077b96dcc8e2a3b6d1662
andre9773 <andre199773@mail.ru>
2019-07-15 12:06:04 +0300

    the and

--- src/js/script.js
+++ src/js/script.js
@@ -16,9 +16,6 @@
 				timer = true
 			},500)}
 		})
-	
-	
-
 
 	window.addEventListener('touchstart', function(e) {
 			var tchs = e.changedTouches[0];
@@ -42,15 +39,53 @@
 					timer = true
 				},300)}
 			}, false);
+
+
+
+
+	wrap = document.getElementById("wrap");
+	pagination__dots = document.getElementsByClassName("pagination__dot");
+	slideCount = Math.abs(positionSector/100);
+
+
+
 	function scrolUp() {
 		if (positionSector >= -100) {
 			positionSector -=100;
 			document.getElementById("page").style.transform = 'translateY(' + positionSector + 'vh)';
+			
+			slideCount = Math.abs(positionSector/100);
+			pagination__dots[slideCount-1].classList.remove("pagination__dot-active");
+			pagination__dots[slideCount].classList.add("pagination__dot-active");
+
+			if (positionSector == -200) {
+				wrap.classList.remove("slideUpUp");
+				wrap.classList.add("slideDownDown");
+				document.getElementById("next").style.cssText = 'transition: 1s; opacity: 0';
+			}else{
+			console.log("scrolUp");
+			wrap.classList.remove("slideDownUp");
+			wrap.classList.add("slideUpUp");
+		}
 		}
 	}
 	function scrolDown() {
 		if (positionSector < 0) {
 			positionSector +=100;
+			
+			slideCount = Math.abs(positionSector/100);
+			pagination__dots[slideCount+1].classList.remove("pagination__dot-active");
+			pagination__dots[slideCount].classList.add("pagination__dot-active");
+
 			document.getElementById("page").style.transform = 'translateY(' + positionSector + 'vh)';
+			document.getElementById("next").style.cssText = 'transition: 1s; opacity: 1';
+
+			if (positionSector == -100) {
+				wrap.classList.remove("slideDownDown");
+				wrap.classList.add("slideUpDown");
+			}else{
+			wrap.classList.remove("slideUpUp", "slideUpDown");
+			wrap.classList.add("slideDownUp");
+			}
 		}
 	}
